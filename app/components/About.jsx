19ff8a28dafd4eafaf051ba2e75323d47d38de4b
Jatto Ade <jattoade@gmail.com>
2017-04-30 14:23:36 +0100

    Styled about Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-// var About = React.createClass({
-//     render:function () {
-//         return(
-//             <h3>About Component</h3>
-//         );
-//     }
-// });
 
 var About = (props) => {
     return(
         <div>
-            <h3>About Component</h3>
-            <p>Welcome to the About Page</p>
+            <h3 className="text-center">About Component</h3>
+            <p>This is a Weather application built on react. I have built this for the complete react Developer Course</p>
+            <p>Here are some of the tools i used</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map API</a> - I used Open Weather Map API to Get the Weather of different locations
+                </li>
+            </ul>
         </div>
     );
 };
